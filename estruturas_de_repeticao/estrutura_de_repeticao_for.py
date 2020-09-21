d2a536e95eb83a772632035746c153d520391620
Mlluiz39 <mlluizpereira39@gmail.com>
2020-09-21 20:04:22 -0300

    condicionais

---
+++ estruturas_de_repeticao/estrutura_de_repeticao_for.py
@@ -0,0 +1,23 @@
+# Vamos testar o codigo
+string = input('Entre com uma sequencia de caracteres: ')
+for caracteres in string:
+    print(caracteres)
+print('...fim...')
+
+# Usando metodos
+string = input('Entre com uma sequencia de caracteres: ')
+for caracteres in string:
+    print(caracteres.upper())
+print('...FIM...')
+
+#for com listas
+lista = ['IGTI', 'Bootcamp', 'Python', 2020]
+for elementos in lista:
+    print(elementos)
+print('...FIM...')
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+for num in lista:
+    print(num)
+print('---FIM---')
+
