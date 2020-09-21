ab4b7f3ecd3829ccb4a75a23f01ee59a29ac6989
Mlluiz39 <mlluizpereira39@gmail.com>
2020-09-21 20:00:44 -0300

    estrutura_de_repeticao_for.py

---
+++ estruturas_de_repeticao/funcao_range.py
@@ -0,0 +1,15 @@
+# Função range com for
+# A função range(), assim como todos os indices começa em 0 e termina em -1
+for i in range(8):
+    print(i)
+print('...FIM...')
+
+for i in [0, 1, 2, 4, 5, 6, 7]:
+    print(i)
+print('...FIM...')
+
+# Definindo intervalos com range()
+for i in range(1, 10):
+    print(i)
+print('...Fim...')
+
