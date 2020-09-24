5e304906b27a3edd8b6fce60fec9f9aea0dd79b1
Mlluiz39 <mlluizpereira39@gmail.com>
2020-09-24 19:06:39 -0300

    pilha_python.py

--- pilha_de_execução_funcoes/pilha_python.py
+++ pilha_de_execução_funcoes/pilha_python.py
@@ -7,7 +7,7 @@ def inverte_string(valor_string):
 
     return resultado
 
-    string = input('Entre com alguma string: ')
-    while string.strip() != '':
-        print('O inverso da ', string, 'é', inverte_string(string))
-        string = input('Entre com outra string ou presione ENTER para sair: ')
+string = input('Entre com alguma string: ')
+while string.strip() != '':
+    print('O inverso da ', string, 'é', inverte_string(string))
+    string = input('Entre com outra string ou presione ENTER para sair: ')
