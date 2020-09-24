28abe82c054c7961b6fde7985071932cdaf3665e
Mlluiz39 <mlluizpereira39@gmail.com>
2020-09-24 19:24:31 -0300

    pilha_python.py

--- pilha_de_execução_funcoes/pilha_python.py
+++ pilha_de_execução_funcoes/pilha_python.py
@@ -11,3 +11,19 @@ string = input('Entre com alguma string: ')
 while string.strip() != '':
     print('O inverso da ', string, 'é', inverte_string(string))
     string = input('Entre com outra string ou presione ENTER para sair: ')
+
+
+# Com esse codigo podemos ver a pinha( stack) em execução usando debug
+
+def soma_dois_numeros(a, b):
+    resultado = a + b
+    return resultado
+
+def multiplica_dois_numeros(c, d):
+    resultado = c * d
+    return resultado
+
+soma = soma_dois_numeros(10, 51)
+print(soma)
+multiplica = multiplica_dois_numeros(5, 8)
+print(multiplica)
