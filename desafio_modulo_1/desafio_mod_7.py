98040cd99976006f89e27dd29d65c0bcb8c200da
Mlluiz39 <mlluizpereira39@gmail.com>
2020-09-29 21:09:55 -0300

    desafio_mod_1.py

---
+++ desafio_modulo_1/desafio_mod_7.py
@@ -0,0 +1,9 @@
+class Classe_2():
+    def __init__(self, l, w):
+        self.a = l
+        self.b = w
+    
+    def metodo_1(self):
+        return self.a * self.b
+
+objeto_1 = Classe_2(12, 10)
